794b90489a76108d9cac9192231eb997e5d1165f
Dion Moraes <91892099+DionMoraes@users.noreply.github.com>
2023-03-24 20:03:43 +0530

    Update brands.php

--- login/brands.php
+++ login/brands.php
@@ -13,6 +13,7 @@
             <script src="https://kit.fontawesome.com/1a25d359d9.js" crossorigin="anonymous"></script>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
             
+        
     </head>
     <body>
         <div>
